c94b8cfb817b2113d500bae0e5c254a19fcf538a
Dani <danitriay@outlook.com>
2019-05-13 21:13:30 +0200

    Render messages emmitted from socket

---
+++ client/main.js
@@ -0,0 +1,21 @@
+var socket = io.connect('http://localhost:6677', {
+  'forceNew': true
+});
+
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+});
+
+function render(data) {
+  var html = data.map(function(message, index) {
+    return (`
+      <div class='message'>
+      <strong> ${message.nickname} </strong> says:
+      <p>${message.text}</p>
+      </div>
+      `);
+  }).join(' ');
+
+  document.getElementById('messages').innerHTML = html;
+}
---
+++ server/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/hello-world', function(req, res){
+  res.status(200).send('Hello world.');
+});
+
+
+var messages = [{
+  id: 1,
+  text: 'Welcome to private chat with NodeJS & socketio of triay0',
+  nickname: 'bot / triay0'
+}];
+
+io.on('connection', function(socket){
+  console.log(socket.handshake.address + ' Has been connected to socket');
+
+  socket.emit('messages', messages);
+});//Metodo encargado de recibir conexiones de clientes y detectar cuando se conecta un cliente
+
+server.listen(6677, function(){
+  console.log('Server running in http://localhost:6677');
+});
